6b593d6df56261217a8d00c19757ca5882b9a951
Andreas Elia <andreaselia@live.co.uk>
2017-01-06 23:42:55 +0000

    Removed a few footer links

--- client/resources/assets/js/components/partials/Footer.vue
+++ client/resources/assets/js/components/partials/Footer.vue
@@ -4,9 +4,7 @@
             <div class="row">
                 <div class="col-sm-10 col-sm-offset-1 text-center">
                     <ul class="list-unstyled list-inline">
-                        <li><a href="#">Our Story</a></li>
                         <li><a href="#">Help/FAQ</a></li>
-                        <li><a href="#">Status</a></li>
                         <li><a href="#">Terms</a></li>
                         <li><a href="#">Privacy</a></li>
                         <li><a href="#">Contact us</a></li>
